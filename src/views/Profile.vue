41940836bff972395c4c192a0448b6bff6f6e781
kabugh <kabugh00@gmail.com>
2020-06-26 22:54:59 +0200

    Profile tabs introduction

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="profile bottomView">
     <div class="profile__container">
-      <section class="hero">
+      <section class="hero shadow-box shadow-14">
         <div class="hero__container">
           <div class="avatar__container">
             <q-avatar color="white" text-color="black" size="4rem">{{
@@ -13,63 +13,118 @@
             <h4>{{ user.email }}</h4>
           </div>
         </div>
-      </section>
-      <div class="details__container">
-        <h3>
-          <span>Dostęp do treningów wygasa:</span>
-          {{ user.premiumAccount.validUntil | date }}
-        </h3>
-        <q-circular-progress
-          show-value
-          font-size="14px"
-          :value="daysLeft"
-          size="5rem"
-          :max="30"
-          :thickness="0.22"
-          color="primary"
-          track-color="grey-3"
-          class="q-ma-md"
+        <q-tabs
+          v-model="currentTab"
+          align="justify"
+          narrow-indicator
+          class="tabs"
         >
-          {{ daysLeft | daysFilter }}
-        </q-circular-progress>
-        <div class="expansionItem__wrapper">
-          <q-expansion-item
-            dense
-            icon="perm_identity"
-            label="Wyświetl ID konta"
-          >
-            {{ user.id }}
-          </q-expansion-item>
-        </div>
-
-        <div class="button__wrapper">
-          <div class="wrapper">
-            <button
-              v-if="!isVerified"
-              class="dark"
-              type="button"
-              @click="verifyAccount"
+          <q-tab
+            v-for="(element, index) in tabs"
+            :name="element.name"
+            :label="element.title"
+            :key="index"
+          />
+        </q-tabs>
+      </section>
+      <q-tab-panels
+        v-model="currentTab"
+        animated
+        swipeable
+        infinite
+        transition-prev="scale"
+        transition-next="scale"
+        class="tab-panels text-center"
+      >
+        <q-tab-panel name="validity"
+          ><div class="details__container">
+            <h3>
+              <span>Dostęp do treningów wygasa:</span>
+              {{ user.premiumAccount.validUntil | date }}
+            </h3>
+            <q-circular-progress
+              show-value
+              font-size="14px"
+              :value="daysLeft"
+              size="5rem"
+              :max="30"
+              :thickness="0.22"
+              color="primary"
+              track-color="grey-3"
+              class="q-ma-md"
             >
-              Zweryfikuj konto
-            </button>
-            <button
-              class="dark"
-              v-show="false"
-              type="button"
-              @click="changeEmail"
-            >
-              Zmień email
-            </button>
-            <button class="dark" type="button" @click="changePassword">
-              Zmień hasło
-            </button>
-            <button class="dark" type="button" @click="logOut">
-              Wyloguj
-            </button>
+              {{ daysLeft | daysFilter }}
+            </q-circular-progress>
+            <q-list>
+              <div class="expansionItem__wrapper">
+                <q-expansion-item
+                  group="accordion"
+                  dense
+                  icon="receipt"
+                  label="Szczegóły umowy"
+                >
+                  <div class="membership__details">
+                    <span>Umowa OxFit na czas nieokreślony</span>
+                  </div>
+                  <button class="dark small-btn" type="button">
+                    Przedłuz karnet
+                  </button>
+                </q-expansion-item>
+              </div>
+              <div class="expansionItem__wrapper">
+                <q-expansion-item
+                  dense
+                  group="accordion"
+                  icon="account_balance_wallet"
+                  label="Stan płatności"
+                >
+                  {{ user.id }}
+                </q-expansion-item>
+              </div>
+            </q-list>
           </div>
-        </div>
-        <ChangeEmailComponent />
-      </div>
+        </q-tab-panel>
+        <q-tab-panel name="settings">
+          <div class="details__container">
+            <div class="button__wrapper">
+              <div class="wrapper">
+                <div class="expansionItem__wrapper">
+                  <q-expansion-item
+                    dense
+                    icon="perm_identity"
+                    label="Wyświetl ID konta"
+                  >
+                    {{ user.id }}
+                  </q-expansion-item>
+                </div>
+                <button
+                  v-if="!isVerified"
+                  class="dark"
+                  type="button"
+                  @click="verifyAccount"
+                >
+                  Zweryfikuj konto
+                </button>
+                <button
+                  class="dark"
+                  v-show="false"
+                  type="button"
+                  @click="changeEmail"
+                >
+                  Zmień email
+                </button>
+                <button class="dark" type="button" @click="changePassword">
+                  Zmień hasło
+                </button>
+                <button class="dark" type="button" @click="logOut">
+                  Wyloguj
+                </button>
+                <ChangeEmailComponent />
+              </div>
+            </div>
+          </div>
+        </q-tab-panel>
+      </q-tab-panels>
     </div>
   </section>
 </template>
@@ -91,6 +146,12 @@ import * as firebase from "firebase";
   }
 })
 export default class Profile extends Vue {
+  tabs = [
+    { title: "Ważność karnetu", name: "validity" },
+    { title: "Ustawienia", name: "settings" }
+  ];
+  currentTab = this.tabs[0].name;
+
   isVerified = false;
   daysLeft = 0;
   created() {
@@ -147,10 +208,13 @@ export default class Profile extends Vue {
       width: 100%;
       height: 30vh;
       @include flex;
+      flex-direction: column;
       align-items: center;
       @include backgroundDefault;
       background-image: url("../assets/bg.jpg");
       color: white;
+      position: relative;
+      z-index: 2;
       .hero__container {
         display: grid;
         column-gap: 5vw;
@@ -167,6 +231,10 @@ export default class Profile extends Vue {
             font-weight: 500;
           }
         }
+      }
+      .tabs {
+        position: absolute;
+        bottom: 0;
       }
     }
     .avatar__wrapper {
@@ -205,6 +273,12 @@ export default class Profile extends Vue {
       .expansionItem__wrapper {
         max-width: 80%;
         margin: 0 auto;
+        padding: 1vh 0;
+        .q-expansion-item__content {
+          padding: 2vh 0;
+          display: grid;
+          row-gap: 2vh;
+        }
       }
       .button__wrapper {
         .wrapper {
